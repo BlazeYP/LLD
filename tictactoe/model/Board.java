4514cb33f404c28b7aac59132a1d94d70693086e
BlazeYP <comfycoding1@gmail.com>
2023-07-15 05:06:22 +0530

    Adding models

---
+++ tictactoe/model/Board.java
@@ -0,0 +1,27 @@
+package tictactoe.model;
+
+public class Board {
+    private int size;
+    private Piece[][] grid;
+
+    public Board(int size) {
+        this.size = size;
+        this.grid = new Piece[size][size];
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Piece[][] getGrid() {
+        return grid;
+    }
+
+    public void setGrid(Piece[][] grid) {
+        this.grid = grid;
+    }
+}

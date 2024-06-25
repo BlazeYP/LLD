022a79ee8fb1bf32d62c829edeaa7ef63f7271f6
BlazeYP <comfycoding1@gmail.com>
2024-06-26 01:38:38 +0530

    Initial Commit | Adding Constants.java

---
+++ game2048/utilities/Constants.java
@@ -0,0 +1,8 @@
+package game2048.utilities;
+
+public interface Constants {
+    int BASE_NUMBER = 2;
+    String SEPARATOR = " ";
+    String EMPTY_CELL_SIGNIFIER = "_";
+    String RESTART = "restart";
+}
